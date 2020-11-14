da070c3e3dbbdc66048d9f439094d3228e45bcf6
Jujubeege <73621007+Jujubeege@users.noreply.github.com>
2020-11-15 01:35:14 +0800

    Create EnemyBehavior.cs

---
+++ Trun/Assets/Scripts/EnemyScripts/EnemyBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public float rotSpeed = 90f;
+    Transform player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(player == null)
+        {
+            GameObject go = GameObject.FindWithTag("Player");
+
+            if(go != null)
+            {
+                player = go.transform;
+            }
+        }
+
+        if (player == null)
+            return;
+
+        Vector3 dir = player.position - transform.position;
+        dir.Normalize();
+
+        float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+
+        Quaternion desiredRot = Quaternion.Euler(0, 0, zAngle);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRot, rotSpeed * Time.deltaTime);
+
+	}
+}
