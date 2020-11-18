815a0c91a5281caabceddae108b34e3ac826b555
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2020-11-18 14:17:42 -0500

    add time based movement

--- Trun/Assets/Scripts/Level/PlatformController.cs
+++ Trun/Assets/Scripts/Level/PlatformController.cs
@@ -6,9 +6,23 @@ public class PlatformController : MonoBehaviour
 {
     [Range(0, 2)]
     public float speed = 1f;
+    public bool positive = false;
+    public float timeMovement = 3f;
+    private float timeDelta = 0f;
     void Update()
-    {
-        transform.Translate(Vector3.down * Time.deltaTime * speed * 2);
+    {   
+        if (timeDelta >= timeMovement) {
+            positive = !positive;
+            timeDelta = 0f;
+        }
+        Vector3 movement;
+        if (positive) {
+            movement = Vector3.up* Time.deltaTime * speed * 2;
+        } else {
+            movement = Vector3.down * Time.deltaTime * speed * 2;
+        }
+        transform.Translate(movement);
+        timeDelta += Time.deltaTime;
         CheckOffScreen();
     }
 
