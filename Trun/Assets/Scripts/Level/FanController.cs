2930bf5bd9632f1839362cad20f5817d31337f0b
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2020-12-02 19:08:19 -0500

    add fan image and script

---
+++ Trun/Assets/Scripts/Level/FanController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanController : MonoBehaviour
+{
+    // private float rotation = 0f;
+    public bool clockwise = true;
+    public float rotationSpeed = 30f;
+    private SpriteRenderer sprite;
+    private void Start() {
+        if (clockwise) {
+            transform.localScale = new Vector3(-1.0f, 1f, 1f);
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime * (clockwise ? 1 : -1));
+    }
+}
