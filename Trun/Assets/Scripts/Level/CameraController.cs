6921d46f75361547a9ebcd74116c464c47d70055
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2020-12-05 14:34:47 -0500

    add some overview scripts to support level needs

---
+++ Trun/Assets/Scripts/Level/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float deltaY = 5f;
+    public float slideUpSpeed = 1f;
+    private float endPosition;
+    private void Start() {
+        endPosition = transform.position.y + deltaY;
+    }
+    void Update() {
+       if (transform.position.y < endPosition){
+           transform.Translate(Vector3.up * slideUpSpeed * Time.deltaTime);
+       }
+   }
+}
---
+++ Trun/Assets/Scripts/Level/LevelController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+    public static float globalGameSpeed = 3f;
+    private float globalTime = 0f;
+    void Start() {
+        
+    }
+    void Update() {
+        globalTime += Time.deltaTime;
+        globalGameSpeed += 0.01f * Time.deltaTime;
+        Debug.Log(globalGameSpeed);
+    }
+}
