d95d2d8efa70dea8a50b2d783ade2d37800d9c90
Jujubeege <73621007+Jujubeege@users.noreply.github.com>
2020-12-09 03:04:02 +0800

    Update PlatformController.cs

--- Trun/Assets/Scripts/Level/PlatformController.cs
+++ Trun/Assets/Scripts/Level/PlatformController.cs
@@ -4,33 +4,56 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
-    [Range(0, 2)]
+    [Range(0, 10)]
     public float speed = 1f;
     public bool positive = false;
-    public float timeMovement = 3f;
-    private float timeDelta = 0f;
+    public float deltaY = 0f;
+    private Transform topPosition;
+    private Transform bottomPosition;
+    void Start() {
+        topPosition =  new GameObject().transform;
+        bottomPosition =  new GameObject().transform;
+            
+        topPosition.Translate(transform.position.x, transform.position.y + deltaY,transform.position.z);
+        bottomPosition.Translate(transform.position.x, transform.position.y - deltaY,transform.position.z);
+    }
     void Update()
     {   
-        if (timeDelta >= timeMovement) {
-            positive = !positive;
-            timeDelta = 0f;
-        }
-        Vector3 movement;
-        if (positive) {
-            movement = Vector3.up* Time.deltaTime * speed * 2;
-        } else {
-            movement = Vector3.down * Time.deltaTime * speed * 2;
-        }
-        transform.Translate(movement);
-        timeDelta += Time.deltaTime;
+        LocalMovement();
+        GlobalMovement();
         CheckOffScreen();
     }
 
+    private void LocalMovement() {
+        // if above top or below bottom, siwthc direction
+        if (transform.position.y >= topPosition.transform.position.y) {
+            positive = false;
+        } else if (transform.position.y <= bottomPosition.transform.position.y) {
+            positive = true;
+        }
+        
+        
+        //move toward top or bottom in local space
+        float step = Time.deltaTime * speed;
+        if (positive) {
+            transform.position = Vector3.MoveTowards(transform.position, topPosition.position, step);
+        } else {
+            transform.position = Vector3.MoveTowards(transform.position, bottomPosition.position, step);
+        }
+    }
 
-
+    private void GlobalMovement() {
+        topPosition.position = new Vector3(topPosition.position.x, topPosition.position.y - LevelController.globalGameSpeed * Time.deltaTime, topPosition.position.z);
+        bottomPosition.position = new Vector3(bottomPosition.position.x, bottomPosition.position.y - LevelController.globalGameSpeed * Time.deltaTime, bottomPosition.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y - LevelController.globalGameSpeed * Time.deltaTime, transform.position.z);
+    }
     private void CheckOffScreen()  {
         if (transform.position.y <= -1f) {
             Destroy(gameObject);
         }
+    } 
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(topPosition.position, bottomPosition.position);
     }
 }
