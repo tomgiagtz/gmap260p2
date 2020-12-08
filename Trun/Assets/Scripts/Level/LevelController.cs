126de48c0b1ed5f9b04a8b5a45b661ce302e3f25
Jujubeege <73621007+Jujubeege@users.noreply.github.com>
2020-12-09 03:03:52 +0800

    Create LevelController.cs

---
+++ Trun/Assets/Scripts/Level/LevelController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+    public static float globalGameSpeed = 1f;
+    [Range (0f, 0.1f)]
+    public float deltaSpeed = 0.01f;
+    public static float globalTime = 0f;
+    void Update() {
+        globalTime += Time.deltaTime;
+        globalGameSpeed += deltaSpeed * Time.deltaTime;
+    }
+}
