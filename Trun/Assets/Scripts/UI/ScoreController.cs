a3dda4a6b892287949e7019b5a3dbf0bb76be464
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2020-12-09 04:03:34 -0500

    add score counter, button prefab, healthbar start

--- Trun/Assets/Scripts/UI/ScoreController.cs
+++ Trun/Assets/Scripts/UI/ScoreController.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreController : MonoBehaviour
 {
+    private TextMeshProUGUI scoreField;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreField = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float score = Mathf.Floor(LevelController.globalTime * 10f);
+        string scoreText = score.ToString();
+        for (int i = 0; scoreText.Length < 4; i++) {
+            scoreText = "0" + scoreText;
+        }
+        scoreField.SetText(scoreText);
     }
 }
